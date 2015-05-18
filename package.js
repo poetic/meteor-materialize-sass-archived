e190dd83feb7f915b10a54399bdc0994cc32e2dc
Chun Yang <yang2007chun@gmail.com>
2015-05-18 13:19:39 -0500

    0.0.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'yang2007chun:materialize-scss',
-  version: '0.0.4',
+  version: '0.0.5',
   // Brief, one-line summary of the package.
   summary: 'materialize sass',
   // URL to the Git repository containing the source code for this package.
