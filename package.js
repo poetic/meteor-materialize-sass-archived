bf0bf9f6761e4eb7bf947d69fdf005b8fa0b1808
Chun Yang <yang2007chun@gmail.com>
2015-10-01 18:06:02 +0800

    1.3.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'poetic:materialize-scss',
-  version: '1.3.0',
+  version: '1.3.3',
   // Brief, one-line summary of the package.
   summary: 'materialize sass',
   // URL to the Git repository containing the source code for this package.
