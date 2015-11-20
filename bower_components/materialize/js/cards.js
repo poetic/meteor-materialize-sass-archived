c91ea395c83825a4e5d1ba0466838d99e31b46bc
Swar Shah <swar12894@gmail.com>
2015-11-19 21:33:38 -0500

    Updated to v0.97.3

--- bower_components/materialize/js/cards.js
+++ bower_components/materialize/js/cards.js
@@ -2,7 +2,7 @@
   $(document).ready(function() {
 
     $(document).on('click.card', '.card', function (e) {
-      if ($(this).find('.card-reveal').length) {
+      if ($(this).find('> .card-reveal').length) {
         if ($(e.target).is($('.card-reveal .card-title')) || $(e.target).is($('.card-reveal .card-title i'))) {
           // Make Reveal animate down and display none
           $(this).find('.card-reveal').velocity(
@@ -16,10 +16,12 @@
         }
         else if ($(e.target).is($('.card .activator')) ||
                  $(e.target).is($('.card .activator i')) ) {
+          $(e.target).closest('.card').css('overflow', 'hidden');
           $(this).find('.card-reveal').css({ display: 'block'}).velocity("stop", false).velocity({translateY: '-100%'}, {duration: 300, queue: false, easing: 'easeInOutQuad'});
         }
       }
 
+      $('.card-reveal').closest('.card').css('overflow', 'hidden');
 
     });
 
